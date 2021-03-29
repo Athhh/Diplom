0e1859a5b73883cfed0f0bb260887d7f538d0272
Athhh <sikalov_dima@mail.ru>
2021-03-29 14:26:23 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -101,7 +101,7 @@ void MainWindow::on_plot_clicked()
     double b = on_rightX_editingFinished();
     double h = on_acc_editingFinished();
 
-//Количество отрисовываемых точек
+//Количество отрисовываемых точекq
 
     double N = (b-a)/(h)+1;
 
